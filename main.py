f2b11176bf03e2de50fe44665a76c55a9e0a58af
YandriJGB <yandrijavier04@gmail.com>
2025-04-01 19:05:41 -0300

    ej. lista de almacen

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hello word")
+# crear lista para almacenar las ventas de la semana 
+ventas = []
+# usar un ciclo para ingresar las ventas 
+for i in range(7): #7 dias de la semana 
+ venta=float (input(F"ingrese la venta del dia {i+1}:"))
+ ventas.append(venta)
+
+
+ #progresar los datos
+ total_ventas = sum(ventas)
+print(total_ventas)
+promedio_ventas = total_ventas/len(ventas)
+
+#condicion para verificar si se alcanzo la meta 
+meta=5000
+if total_ventas >= meta:
+ mensaje = "felicidades has alcanzado la meta"
+else:
+ mensaje = "no has alcanzado la meta )"
+
+ #imprimir resultado 
+print ("\n---reporte---")
+print (f"total de ventas: ${total_ventas}")
+print (f"promedio de ventas: ${promedio_ventas}")
+print(mensaje)
